79df3ee0f886b1b7ecaa9e5b88aad83f0655ea55
AurelOdjo <xgeosoft@gmail.com>
2023-12-27 15:15:02 +0100

    double

--- R/base.R
+++ R/base.R
@@ -4,3 +4,10 @@ standard_deviation = function(x,for.sample = TRUE){
   if(for.sample) return(sd(x))
   else return(sqrt(var(x)))
 }
+
+
+standard_deviation = function(x,for.sample = TRUE){
+  cat("\n=== Well ===\n")
+  if(for.sample) return(sd(x))
+  else return(sqrt(var(x)))
+}
